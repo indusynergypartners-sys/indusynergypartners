af0119858e339d04d3e835e6f4ba48c8da8c51d6
indusynergypartners-sys <indusynergypartners@gmail.com>
2025-08-25 19:16:30 +0530

    Update Blog.jsx

--- src/admin/Blog.jsx
+++ src/admin/Blog.jsx
@@ -8,8 +8,8 @@ import EditIcon from '@mui/icons-material/Edit';
 import { useNavigate } from 'react-router-dom';
 import axios from 'axios';
 
-const API_BASE = 'https://blog-backend-1-p3yg.onrender.com/api';
-const API_BASE_IMG = 'https://blog-backend-1-p3yg.onrender.com';
+const API_BASE = 'https://backend-dw1x.onrender.com/api';
+const API_BASE_IMG = 'https://backend-dw1x.onrender.com';
 
 export default function Blog() {
   const [blogs, setBlogs] = useState([]);
